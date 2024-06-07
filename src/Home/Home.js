e65d78770793ae0772bdfc6a8f9c95fd0a466ef4
Nadun Dissanayakke <139664686+Nadun-Dissanayake@users.noreply.github.com>
2024-06-07 10:08:34 +0530

    second commit

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -60,21 +60,35 @@ function Home() {
                 <div class="service-content">
                     <p>Real Solution, Real Fast!</p>
                     <h1>Best Global Logistics Solution</h1>
-                    <div class="diferent-services">
-                        <div class="air-cargo">
-                            <img class="air-cargo-img" src={aircargo} />
-                            <div class="air-cargo-text">
-                                <h2>Air Freight Service</h2>
-                                <p>At our Auto Service garage, we are fully appreciate how difficult occur it is for people to find.</p>
-                                <button>
-                                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="m560-240-56-58 142-142H160v-80h486L504-662l56-58 240 240-240 240Z"/></svg>
-                                    <p>Read More</p>
-                                </button>
+                    <div class="different-service-container">
+                        <div class="diferent-services">
+                            <div class="air-cargo">
+                                <img class="air-cargo-img" src={aircargo} />
+                                <div class="air-cargo-text">
+                                    <h2>Air Freight Service</h2>
+                                    <p>At our Auto Service garage, we are fully appreciate how difficult occur it is for people to find.</p>
+                                    <button>
+                                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="m560-240-56-58 142-142H160v-80h486L504-662l56-58 240 240-240 240Z"/></svg>
+                                        <p>Read More</p>
+                                    </button>
+                                </div>
+                            </div>
+                            <div class="air-cargo">
+                                <img class="air-cargo-img" src={aircargo} />
+                                <div class="air-cargo-text">
+                                    <h2>Air Freight Service</h2>
+                                    <p>At our Auto Service garage, we are fully appreciate how difficult occur it is for people to find.</p>
+                                    <button>
+                                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="m560-240-56-58 142-142H160v-80h486L504-662l56-58 240 240-240 240Z"/></svg>
+                                        <p>Read More</p>
+                                    </button>
+                                </div>
                             </div>
                         </div>
-                        <div class="sea-cargo">
-
-                        </div>
+                    </div>
+                    <div class="transport-solution">
+                        <p>Logistic & Transport Solution Saves Your Time. <span>Find your Solutions</span></p>
+                        <svg xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 -960 960 960" width="18px" fill="#5f6368"><path d="m560-240-56-58 142-142H160v-80h486L504-662l56-58 240 240-240 240Z"/></svg>
                     </div>
                     <div>
                         <div>
